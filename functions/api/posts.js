100b35b4c24a59602a105637a6044b2158badd9f
Navneet Kaur <navneetkaur@tactica.ca>
2023-07-14 13:33:12 -0500

    Trying a post request.

--- functions/api/posts.js
+++ functions/api/posts.js
@@ -1,27 +1,26 @@
-export async function onRequestPost({ request, env }) {
-  return await submitHandler({ request, env });
-};
-
-async function submitHandler ({ request, env }) {
+export default {
+  async fetch(request) {
     const corsHeaders = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-      "Access-Control-Max-Age": "86400",
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+      'Access-Control-Max-Age': '86400'
     };
 
-    // The URL for the remote third party API you want to fetch from
-    // but does not implement CORS
-    const API_URL = "https://examples.cloudflareworkers.com/demos/demoapi";
+    /*
+     * The URL for the remote third party API you want to fetch from
+     * but does not implement CORS
+     */
+    const API_URL = 'https://examples.cloudflareworkers.com/demos/demoapi';
 
     // The endpoint you want the CORS reverse proxy to be on
-    const PROXY_ENDPOINT = "/corsproxy/";
+    const PROXY_ENDPOINT = '/corsproxy/';
 
     // The rest of this snippet for the demo page
     function rawHtmlResponse(html) {
       return new Response(html, {
         headers: {
-          "content-type": "text/html;charset=UTF-8",
-        },
+          'content-type': 'text/html;charset=UTF-8'
+        }
       });
     }
 
@@ -78,75 +77,78 @@ async function submitHandler ({ request, env }) {
 
     async function handleRequest(request) {
       const url = new URL(request.url);
-      let apiUrl = url.searchParams.get("apiurl");
+      let apiUrl = url.searchParams.get('apiurl');
 
       if (apiUrl == null) {
         apiUrl = API_URL;
       }
 
-      // Rewrite request to point to API URL. This also makes the request mutable
-      // so you can add the correct Origin header to make the API server think
-      // that this request is not cross-site.
+      /*
+       * Rewrite request to point to API URL. This also makes the request mutable
+       * so you can add the correct Origin header to make the API server think
+       * that this request is not cross-site.
+       */
       request = new Request(apiUrl, request);
-      request.headers.set("Origin", new URL(apiUrl).origin);
+      request.headers.set('Origin', new URL(apiUrl).origin);
       let response = await fetch(request);
       // Recreate the response so you can modify the headers
 
       response = new Response(response.body, response);
       // Set CORS headers
 
-      response.headers.set("Access-Control-Allow-Origin", url.origin);
+      response.headers.set('Access-Control-Allow-Origin', '*');
 
       // Append to/Add Vary header so browser will cache response correctly
-      response.headers.append("Vary", "Origin");
+      response.headers.append('Vary', 'Origin');
 
       return response;
     }
 
     async function handleOptions(request) {
       if (
-        request.headers.get("Origin") !== null &&
-        request.headers.get("Access-Control-Request-Method") !== null &&
-        request.headers.get("Access-Control-Request-Headers") !== null
+        request.headers.get('Origin') !== null &&
+        request.headers.get('Access-Control-Request-Method') !== null &&
+        request.headers.get('Access-Control-Request-Headers') !== null
       ) {
         // Handle CORS preflight requests.
         return new Response(null, {
           headers: {
             ...corsHeaders,
-            "Access-Control-Allow-Headers": request.headers.get(
-              "Access-Control-Request-Headers"
-            ),
-          },
-        });
-      } else {
-        // Handle standard OPTIONS request.
-        return new Response(null, {
-          headers: {
-            Allow: "GET, HEAD, POST, OPTIONS",
-          },
+            'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers')
+          }
         });
       }
+
+      // Handle standard OPTIONS request.
+      return new Response(null, {
+        headers: {
+          Allow: 'GET, HEAD, POST, OPTIONS'
+        }
+      });
     }
 
     const url = new URL(request.url);
+
     if (url.pathname.startsWith(PROXY_ENDPOINT)) {
-      if (request.method === "OPTIONS") {
+      if (request.method === 'OPTIONS') {
         // Handle CORS preflight requests
         return handleOptions(request);
-      } else if (
-        request.method === "GET" ||
-        request.method === "HEAD" ||
-        request.method === "POST"
+      }
+      else if (
+        request.method === 'GET' ||
+        request.method === 'HEAD' ||
+        request.method === 'POST'
       ) {
         // Handle requests to the API server
         return handleRequest(request);
-      } else {
-        return new Response(null, {
-          status: 405,
-          statusText: "Method Not Allowed",
-        });
       }
-    } else {
-      return rawHtmlResponse(DEMO_PAGE);
+
+      return new Response(null, {
+        status: 405,
+        statusText: 'Method Not Allowed'
+      });
     }
+
+    return rawHtmlResponse(DEMO_PAGE);
+  }
 };
