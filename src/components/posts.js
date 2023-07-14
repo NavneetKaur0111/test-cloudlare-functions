c0d2788288c914aca2af45f7d58227175552b229
Navneet Kaur <navneetkaur@tactica.ca>
2023-07-14 09:47:37 -0500

    Trying a post request.

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import axios from "axios";
 
 const Post = () => {
   const [post, setPost] = useState({});
+  const [error, setError] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     const getPost = async () => {
-      const resp = await fetch(`/api/post/?apiurl=https://redcap.ualberta.ca/api/`);
+      const data = new FormData();
+      data.append('token', process.env.REACT_APP_REDCAP_TOKEN);
+      data.append('format', 'json');
+      data.append('content', 'record');
+      data.append('type', 'flat');
+      data.append('data', JSON.stringify([{record_id: 'testinggggg'}]));
+      const resp = axios('/api/post/?apiurl=https://redcap.ualberta.ca/api/', data)
+        .then(result => {
+          setPost(result);
+          return resolve(result)
+        })
+        .catch(error => {
+          setError(error);
+          return reject(error)
+        });
+
+      // const resp = await fetch(`/api/post/?apiurl=https://redcap.ualberta.ca/api/`);
       const postResp = await resp.json();
       setPost(postResp);
     };
@@ -20,6 +38,7 @@ const Post = () => {
   return (
     <div>
       <h1>{post}</h1>
+      <h2>{error}</h2>
       <p>
         <em>Published {new Date(post.published_at).toLocaleString()}</em>
       </p>
