ac66bb9b6d5991e98c7a269871589e86e2b5cfaa
Navneet Kaur <navneetkaur@tactica.ca>
2023-07-14 17:07:41 -0500

    WIP14

--- functions/api/redcap.js
+++ functions/api/redcap.js
@@ -1,5 +1,3 @@
 export function onRequest(request) {
-  return fetch('https://redcap.ualberta.ca/api/', {
-    data: [{record_id: '1'}]
-  })
+  return fetch('https://redcap.ualberta.ca/api/', request)
 }
